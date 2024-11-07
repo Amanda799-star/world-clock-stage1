d1116febdd666a65745eeeca3083932d59053c8f
Amanda799-star <amandandalatheone@gmail.com>
2024-11-07 10:59:04 +0200

    added js

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-
+function timeInterval(){
 let losDate = moment().tz("America/Los Angeles").format("MMMM D YYYY");
 let losDateElement = document.querySelector("#losDate");
 losDateElement.innerHTML = losDate;
 
-let losTime = moment().tz("America/Los Angeles").format("H:m:s");
+let losTime = moment().tz("America/Los Angeles").format("H:m:ss");
 let timeElement = document.querySelector("#losTime");
-timeElement.innerHTML = losTime;
+timeElement.innerHTML = `${losTime}<small>AM</small>`;
 
-let paDate = moment().tz("America/Los Angeles").format("MMMM D YYYY");
-let losDateElement = document.querySelector("#losDate");
-losDateElement.innerHTML = losDate;
 
-let paTime = moment().tz("Europe/Paris").format("H:m:s");
-let timeElement = document.querySelector("#losTime");
-timeElement.innerHTML=losTime
+let joDate = moment().tz("Africa/Johannesburg").format("MMMM D YYYY");
+let joDateElement = document.querySelector("#joDate");
+joDateElement.innerHTML = losDate;
+
+let joTime = moment().tz("Africa/Johannesburg").format("H:m:ss ");
+let joTimeElement = document.querySelector("#joTime");
+joTimeElement.innerHTML=`${losTime}<small>AM</small>`;
+
+
+let canada=moment().tz("Canada/Central").format("MMMM D YYYY");
+let canDate=document.querySelector("#caDate");
+canDate.innerHTML=canada;
+
+let canadaTime=moment().tz("Canada/Central").format("h:m:ss");
+let canTime=document.querySelector("#caTime");
+canTime.innerHTML=`${canadaTime}<small>AM</small>`;
+}
+setInterval(timeInterval,1);
